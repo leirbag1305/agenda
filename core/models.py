d52d129badd65b97c5a09b766a363cde6eda6f47
leirbag1305 <ghfs.15@gmail.com>
2021-03-30 15:35:33 -0300

    Criado tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -10,7 +10,10 @@ class Evento(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'evento'
+        db_table = 'evento'     #Renomea a tabela de core_evento para evento
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
